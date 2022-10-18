79199da563e0158f0254fadf5cc4bf6041ff3371
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-18 13:44:46 +0800

    feat: Implemeted Search function using custom search class and downloaded algolia.

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,7 +1,32 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
 user = settings.AUTH_USER_MODEL
+
+
+# to implement a custom qureyset we need to create a class that inherits from models.QuerySet
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+
+    def search(self, query, user):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            # we want to return all the products that are public or not public but belong to the user
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        # We return the custom queryset
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().search(query, user=user)
 
 
 class Product(models.Model):
@@ -11,6 +36,8 @@ class Product(models.Model):
     price = models.PositiveIntegerField(default=19)
     # Check if product is available for public to view
     public = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     @property
     def discount_price(self):
--- backend/search/views.py
+++ backend/search/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class SearchListView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        # get the query from the url
+        search_results = Product.objects.none()
+        query = self.request.GET.get('query')
+        # only return results if the query is not empty
+        if query is not None:
+            user = None
+            if self.request.user.is_authenticated:
+                user = self.request.user
+
+            search_results = queryset.search(query, user=user)
+        return search_results
